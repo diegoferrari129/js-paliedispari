6de129c2c5babd961a2db8fdcfcacd1911361713
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-05 15:52:37 +0200

    [UPD] created a prompt and started a cycle for

--- js/script.js
+++ js/script.js
@@ -3,4 +3,15 @@ function palindroma(word) {
 
     let inverseWord = '';
 
+    for (let i = word.length -1 ; i >= 0; i--){
+
+        inverseWord += word[i];
+
+    }
+
+    
 }
+
+const userInput = prompt('Inserisci una parola');
+
+palindroma(userInput.toLowerCase());
