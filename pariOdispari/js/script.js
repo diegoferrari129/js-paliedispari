be9b87ea46e742c4c2b2dff13936af262c98a997
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-05 18:36:46 +0200

    [UPD] created functions randomNumber and evenOrOdd

---
+++ pariOdispari/js/script.js
@@ -0,0 +1,15 @@
+function randomNumber(){
+    return Math.floor(Math.random() * 5 + 1);
+}
+
+function evenOrOdd(sum){
+    if (sum % 2 === 0) {
+        return 'pari'
+    }
+
+    else {
+        return 'dispari'
+    }
+
+}
+
